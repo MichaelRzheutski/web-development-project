8fea9896a89a78c324d9744ddb5ace43d3e2ec85
MichalRzavucki <michael.rzheutski@gmail.com>
2021-04-12 00:31:24 +0300

    Team section created

--- js/main.js
+++ js/main.js
@@ -56,4 +56,27 @@ $(document).ready(function () {
       }
    });
 
+   /* Team Slider */
+   $('.multiple-items').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      arrows: false,
+      dots: true,
+      dotsClass: 'dots-style',
+      responsive: [{
+         breakpoint: 1200,
+         settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+         }
+      }, {
+         breakpoint: 768,
+         settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+         }
+      }]
+   });
+
 });
